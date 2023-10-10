7a312757ee8afc7cb269114d699a3f845e47c796
lacie-life <00sao00ios00@gmail.com>
2023-10-10 19:29:46 +0900

    [Update] Add data explain

--- Neo4jAPI.py
+++ Neo4jAPI.py
@@ -1,5 +1,22 @@
 from neo4j import GraphDatabase
 
+### Entity type:
+# • Organization:
+# • Time
+# • Product
+# • Promotion
+# • Regulation
+# • Location
+# • Price
+###
+
+### Relation type
+# Affiliation
+# Regulation
+# Location
+# Price
+###
+
 class Neo:
     def __init__(self, username, pwd):
         self.driver = GraphDatabase.driver(uri="neo4j://localhost:7687", auth=(username, pwd))
@@ -13,38 +30,42 @@ class Neo:
             # Write transactions allow the driver to handle retries and transient errors
             result = session.write_transaction(
                 self._CREATE, node_name, label)
+
     @staticmethod
     def _CREATE(tx, node_name, label):
-        neo = Neo("neo4j","1")
-        #print(node_name + " " + label)
-        if (label=="medical_specality"):
-            if (not neo.MATCH(list(node_name))):
-                print("Node exist")
-                return
-            else :
-                #print("Creating")
-                cmd = (
-                "CREATE (w:Medical_specality {name: $node_name}) "
-                )
-                temp = tx.run(cmd, label=label, node_name=node_name)
-        elif (label == "symptom"):
-            if (not neo.MATCH(list(node_name))):
+        neo = Neo("neo4j", "1")
+        # print(node_name + " " + label)
+        if label == "medical_specality":
+            if not neo.MATCH(list(node_name)):
                 print("Node exist")
                 return
             else:
-                #print("Creating")
+                # print("Creating")
                 cmd = (
-                "CREATE (n:Symptom {name: $node_name}) "
+                    "CREATE (w:Medical_specality {name: $node_name}) "
+                )
+                temp = tx.run(cmd, label=label, node_name=node_name)
+        elif label == "symptom":
+            if not neo.MATCH(list(node_name)):
+                print("Node exist")
+                return
+            else:
+                # print("Creating")
+                cmd = (
+                    "CREATE (n:Symptom {name: $node_name}) "
                 )
             temp = tx.run(cmd, label=label, node_name=node_name)
 
-    def create_relation(self, node_1, node_2):
+    def create_relation(self, node_1, node_2, relation):
         with self.driver.session() as session:
             # Write transactions allow the driver to handle retries and transient errors
             result = session.write_transaction(
-                self._create_relation, node_1,node_2)
+                self._create_relation, node_1, node_2, relation)
+
     @staticmethod
-    def _create_relation(tx, node_1, node_2):
+    def _create_relation(tx, node_1, node_2, relation):
+
+        # Check relation type
 
         cmd = (
             "MATCH "
@@ -53,8 +74,7 @@ class Neo:
             "where n.name = $node_1 AND w.name = $node_2 "
             "create p = (w)-[r:SYMPTOM]->(n) "
             "return p")
-        temp = tx.run(cmd,  node_1=node_1, node_2=node_2)
-
+        temp = tx.run(cmd, node_1=node_1, node_2=node_2)
 
     def CREATE(self, sentence):
         with self.driver.session() as session:
@@ -63,92 +83,98 @@ class Neo:
                 self.m_create, sentence)
 
     @staticmethod
-    def m_create (tx, sentence):
+    def m_create(tx, sentence):
         temp = sentence.lower()
         texts = temp.split()
-        for i in range(0, len(texts)-1):
+        for i in range(0, len(texts) - 1):
             cmd = (
-                    "merge (w1:Word{name:$tx}) "
-                    "on create set w1.count = 1 on match set w1.count = w1.count +1 "     
-                    "merge (w2:Word{name:$tx2}) "
-                    "on create set w2.count = 1 on match set w2.count = w2.count +1 "
-                    "merge (w1)-[r:NEXT]->(w2) "
-                    "on create set r.count =1 "
-                    "on match set r.count = r.count +1;"
+                "merge (w1:Word{name:$tx}) "
+                "on create set w1.count = 1 on match set w1.count = w1.count +1 "
+                "merge (w2:Word{name:$tx2}) "
+                "on create set w2.count = 1 on match set w2.count = w2.count +1 "
+                "merge (w1)-[r:NEXT]->(w2) "
+                "on create set r.count =1 "
+                "on match set r.count = r.count +1;"
             )
-            #print(cmd)
-            tx.run(cmd, sz=len(texts) - 2, tx=texts[i], tx2=texts[i+1])
+            # print(cmd)
+            tx.run(cmd, sz=len(texts) - 2, tx=texts[i], tx2=texts[i + 1])
 
     def MATCH(self, key):
         with self.driver.session() as session:
             result = session.write_transaction(
                 self.m_match, key
-           )
+            )
             return result
+
     @staticmethod
     def m_match(tx, key):
         cmd = (
-               "MATCH (w:Word) "
-               "where w.name = $key "
-               "return *;"
+            "MATCH (w:Word) "
+            "where w.name = $key "
+            "return *;"
         )
-        temp = tx.run(cmd, key = key)
+        temp = tx.run(cmd, key=key)
         return temp
+
     def UPDATE(self, new_properties):
         with self.driver.session() as session:
             result = session.write_transaction(
                 self.m_update, new_properties
             )
+
     @staticmethod
     def m_update(tx, new_properties):
         cmd = (
             "match (w:Word {name: $name}) "
             "set w = $new_prop")
-        temp = tx.run(cmd, new_prop = new_properties, name = new_properties['name'])
+        temp = tx.run(cmd, new_prop=new_properties, name=new_properties['name'])
+
     def DELETE(self, delete):
         with self.driver.session() as session:
             result = session.write_transaction(
-                self.m_delete,delete)
+                self.m_delete, delete)
+
     @staticmethod
     def m_delete(tx, delete):
-         if (delete['cmd'] == 'delete_nodes'):
+        if delete['cmd'] == 'delete_nodes':
             for node in delete['node']:
-             cmd = (
-                "match (w:Word {name: $name})"
-                "detach delete w"
-             )
-             tx.run(cmd, name=node)
-         elif (delete['cmd'] =='delete_all'):
-             cmd = (
+                cmd = (
+                    "match (w:Word {name: $name})"
+                    "detach delete w"
+                )
+                tx.run(cmd, name=node)
+        elif delete['cmd'] == 'delete_all':
+            cmd = (
                 "match (n)"
                 "detach delete n"
-              )
-             tx.run(cmd)
-         elif (delete['cmd'] == 'greater'):
-             cmd = (
-                 "match (w:Word) "
-                 "where w.count > $count "
-                 "detach delete w"
-             )
-             tx.run(cmd,count=delete['count'])
-         elif (delete['cmd'] == "equal"):
-             cmd = (
-                 "match (w:Word) "
-                 "where w.count = $count "
-                 "detach delete w"
-             )
-             tx.run(cmd,count=delete['count'])
-         elif (delete['cmd'] == "lesser"):
-             cmd = (
-                 "match (w:Word) "
-                 "where w.count < $count "
-                 "detach delete w"
-             )
-             tx.run(cmd,count=delete['count'])
+            )
+            tx.run(cmd)
+        elif delete['cmd'] == 'greater':
+            cmd = (
+                "match (w:Word) "
+                "where w.count > $count "
+                "detach delete w"
+            )
+            tx.run(cmd, count=delete['count'])
+        elif delete['cmd'] == "equal":
+            cmd = (
+                "match (w:Word) "
+                "where w.count = $count "
+                "detach delete w"
+            )
+            tx.run(cmd, count=delete['count'])
+        elif delete['cmd'] == "lesser":
+            cmd = (
+                "match (w:Word) "
+                "where w.count < $count "
+                "detach delete w"
+            )
+            tx.run(cmd, count=delete['count'])
+
     def match_check(self, key):
         with self.driver.session() as session:
             result = session.read_transaction(
-                self.result,key)
+                self.result, key)
         return result
 
     @staticmethod
@@ -160,9 +186,7 @@ class Neo:
         entire_result = []  # Will contain all the items
         for record in temp:
             entire_result.append(record.value())
-        if(len(entire_result) == 0 ):
+        if len(entire_result) == 0:
             return False
         else:
             return entire_result
-
-
--- main.py
+++ main.py
@@ -1,32 +1,47 @@
 import re
 
 node_1_start = "[E1]"
-node_1_end= "[/E1]"
+node_1_end = "[/E1]"
 
 node_2_start = "[E2]"
-node_2_end= "[/E2]"
+node_2_end = "[/E2]"
 
 i = 0
 
+
 def edgeExtraction(sample):
-    print(sample)
+    # print(sample)
     x = sample.replace('\t', ' ').replace('\n', '')
     tmp = x.split(" ")
     print(tmp)
     node_1 = ''
+    node_1_label = ''
     node_2 = ''
+    node_2_label = ''
     edge = tmp[0]
 
-    print(x.find(node_1_start))
-    print(x.find(node_1_end))
+    if edge == 'OTHER':
+        return
+
+    # print(x.find(node_1_start))
+    # print(x.find(node_2_start))
+
+    if x.find(node_1_start) < x.find(node_2_start):
+        node_1_label = tmp[5]
+        node_2_label = tmp[6]
+    else:
+        node_1_label = tmp[6]
+        node_2_label = tmp[5]
 
     node_1 = x[x.find(node_1_start) + len(node_1_start):x.find(node_1_end)]
     node_2 = x[x.find(node_2_start) + len(node_2_start):x.find(node_2_end)]
 
     print("index: " + str(i))
     print("edge: " + edge)
-    print("node_1: " + node_1)
-    print("node_2: " + node_2)
+    print("node_1: " + node_1 + "| label: " + node_1_label)
+    print("node_2: " + node_2 + "| label: " + node_2_label)
+
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -34,12 +49,10 @@ if __name__ == '__main__':
     f = open("train-readble.txt", "r")
 
     for x in f:
-        edgeExtraction(f.readline())
+        if len(x) > 0:
+            edgeExtraction(x)
         i = i + 1
 
     f.close()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
-
