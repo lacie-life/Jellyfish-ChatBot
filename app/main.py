a78d9dc81cb27e7a8cb4202629a0fcb2419975bb
lacie-life <00sao00ios00@gmail.com>
2023-12-05 22:41:59 +0900

    [Update]

--- app/main.py
+++ app/main.py
@@ -34,19 +34,27 @@ MERGE (u:User {id: $userId})
 st.set_page_config(layout="wide")
 st.title("Jellyfish Chatbot")
 
-responses = ["Tại Winmart đang có các sản phẩm nho đen mỹ, cam vàng úc, cam sành hàm yên, mận đá. Bạn cũng có thể tham khảo nước ngọt coca cola 235ml, gạo st25 vinaseed 3kg hay dầu đậu nành simply 2l. Đến ngay cửa hàng Winmart để biết thêm các ưu đãi cho các sản phẩm.",
-             "Tại Winmart đang có các sản phẩm Nước giặt xả Golden king hương ban mai 3,6l giá 89.500đ/can, hộp thực phẩm Hokkaido chữ nhật/tròn 1000ml giá 51.500đ/cái. Ngoài ra, bánh trứng Karo chà bông/chà bông phô mai 26g: 27.800đ/túi, hạt nêm Chin-su thơm ngọt thịt 1kg: 42.900đ/gói và Socola Ferrero rocher 200g: 181.900đ/hộp. Cùng nhiều sản phẩm với mức giá hấp dẫn như 63.900đ/lốc giấy vệ sinh alavie blessyou 10 cuộn 2 lớp, 41.800đ/hộp màng bọc thực phẩm pe 30*100m hay 173.000đ/túi nước giặt ariel cửa trước downy oải hương 3.7kg. Ghé cửa hàng Winmart để mua sắm với mức giá ưu đãi.",
-             "Hiện tại 7-eleven đang bán những sản phẩm hấp dẫn sau. Bạn có thể tham khảo bưởi da xanh tách múi, combo bánh mỳ, cơm trắng/cơm chiên. Ngoài ra còn có bánh mì cải bó xôi xá xíu, hồng trà sữa/trà sữa oolong rang ly lớn hay fuzetea+. Cuối cùng, bạn có thể mua trà sữa, trà tắc khổng lồ hay combo mì trộn tại 7-eleven. Tới cửa hàng 7-eleven để có thể xem thêm nhiều sản phẩm và ưu đãi hấp dẫn.",
-             "Tại 7-eleven đang có các sản phẩm Clamelo trà tắc bưởi hồng giá dùng thử chỉ 22k, Cà phê sữa tươi 13k. Ngoài ra, mì trộn giá hẹ có giá 14k, nho đỏ úc 500gr có giá 102k hay cháo ngêu pattaya có giá 25k. Cùng nhiều sản phẩm với mức giá hấp dẫn như 79k mận Hà Nội 500g, 30k gỏi lạp thái hay 213k táo fuji Nhật (hộp 3 trái). Ghé cửa hàng Winmart để mua sắm với mức giá ưu đãi."]
+responses = ["Sau đây là giá của một số sản phẩm tại 7-eleven bạn có thể tham khảo: \n - Cà phê kem muối có giá 20k, 25k cho trà đào/ trà vải/ trà nhãn mật ong. \n - 660k 1 chai chống nắng senka uv milk. \n - 125k cherry mỹ hộp 250gr. \n \n Ngoài ra, chỉ 20k bánh mì pate, từ 300k hộp quà trái cây. Bạn cũng có thể tham khảo dâu tây hàn quốc 250gr: 129k hay bánh su kem xoài 14k/hộp.",
+             "Cảm ơn bạn đã quan tâm, hiện 7-eleven còn nhiều sản phẩm với mức giá hấp dẫn khác như: \n - 41k trứng gà ta v.food 10 quả. \n - 19k khóm cắt sẵn. \n Ngoài ra còn có hộp sushi inari lục bảo 40k, trà tắc khổng lồ 10k hay trà oolong nhài sữa từ 20k. \n \n  Ngoài ra, 116k táo breeze túi 1kg, 29k burger trứng jambon phô mai cùng nhiều sản phẩm khác. \n \n Ghé 7-eleven để mua sắm với mức giá ưu đãi.",
+             "Giá bán của một số sản phẩm tại Winmart: \n - Lê Hàn Quốc nhập khẩu 64.900đ/kg. \n - Bánh quy cosy thập cẩm socola/rắc hạt 336g: 74.500đ/hộp. \n - Bánh Danisa gold 279.000đ/hộp \n - 149.900đ/hộp 250gr dâu hàn quốc. \n - 199.900đ/kg nho đen mỹ. \n \n Bạn cũng có thể tham khảo mận đá: 39.900đ/kg, bưởi diễn: 19.900đ/quả, bia Carlsberg 330ml: 462.000đ/thùng cùng nhiều sản phẩm khác.",
+             "Cảm ơn bạn đã quan tâm, hiện hệ thống cửa hàng Winmart còn nhiều sản phẩm với mức giá hấp dẫn khác như: \n - 159.000đ/chai dầu gội clear 9 loại thảo dược sạch gàu 630g. - \n 49.000đ/lốc giấy vệ sinh winmart home 10 cuộn 3 lớp không lõi. \n - 24.500đ/hộp kim chi dưa cải bibigo. \n - 9.300đ/lon nước giải khát coca 330/320ml. \n - 29.900đ/chai nước mắm chinsu hương cá hồi 500ml. \n \n Cùng nhiều sản phẩm khác với mức giá hấp dẫn như 23.800đ/lốc 4 sữa trái cây lif kin hương cam/nho 180ml, 12.000đ/hộp bánh dinh dưỡng afc vị caramel flan 125g. \n \n Ghé hệ thống cửa hàng Winmart để mua sắm với mức giá ưu đãi.",
+             "Hiện tại, 7-eleven đang có một số khuyến mãi hấp dẫn. Bạn có thể nhận được một túi 7-eleven miễn phí, hoặc một chai Nutriboost cam/dâu 297ml miễn phí. Ngoài ra, bạn còn có cơ hội được tặng gối bông tea+ oolong size lớn, tặng nutriboost cookies hoặc tặng ly rap việt. Ngoài ra, còn có các khuyến mãi giảm giá từ 3k đến 14k hay các chương trình đồng giá 20k. Cuối cùng, bạn cũng có thể nhận được một topping miễn phí.  Ghé 7-eleven để mua sắm với mức giá ưu đãi.",
+             "Tại chuỗi cửa hàng Winmart đang có các chương trình khuyến mãi sau. Khuyến mãi giảm giá lên tới 33%, khuyến mãi mua 1 tặng 1. Ngoài ra, bạn còn có cơ hội nhận ngay combo quà ly giữ nhiệt và gối tựa cổ, nhận ngay một bộ lì xì xịn mịn. Ngoài ra, còn có các khuyến mãi giảm giá 50%, giảm sốc 20%. Cuối cùng, bạn có thể được tặng thêm 1 lon strongbow đào.  Ghé Winmart để mua sắm với mức giá ưu đãi"]
 
-cypher_output = ["MATCH path = (a:Property {value: 'product_winmart'})-[:INIT]->(b:Product) RETURN path",
-                 "MATCH path = (a:Property {value: 'product_winmart'})-[:INIT]->(b:Product)-[:PRICE]->(c:Price) RETURN path",
-                 "MATCH path = (a:Property {value: 'product_711'})-[:INIT]->(b:Product) RETURN path",
-                 "MATCH path = (a:Property {value: 'product_711'})-[:INIT]->(b:Product)-[:PRICE]->(c:Price) RETURN path"]
+cypher_output = ["CALL {MATCH path = (c:Property {value: 'product_711'})-[:INIT]->(d:Product) RETURN path LIMIT 10} CALL {MATCH path2 = (a:Property {value: 'price_711'})-[:INIT]->(b:Price) RETURN path2 LIMIT 10} RETURN path, path2",
+                 "CALL {MATCH path = (c:Property {value: 'product_711'})-[:INIT]->(d:Product) RETURN path LIMIT 30} CALL {MATCH path2 = (a:Property {value: 'price_711'})-[:INIT]->(b:Price) RETURN path2 LIMIT 30} RETURN path, path2",
+                 "CALL {MATCH path = (c:Property {value: 'product_winmart'})-[:INIT]->(d:Product) RETURN path LIMIT 10} CALL {MATCH path2 = (a:Property {value: 'price_winmart'})-[:INIT]->(b:Price) RETURN path2 LIMIT 10} RETURN path, path2",
+                 "CALL {MATCH path = (c:Property {value: 'product_winmart'})-[:INIT]->(d:Product) RETURN path LIMIT 30} CALL {MATCH path2 = (a:Property {value: 'price_winmart'})-[:INIT]->(b:Price) RETURN path2 LIMIT 30} RETURN path, path2",
+                 "CALL {MATCH path = (c:Property {value: 'promotion_711'})-[:INIT]->(d:Promotion) RETURN path LIMIT 10} CALL {MATCH path2 = (a:Property {value: 'product_711'})-[:INIT]->(b:Product) RETURN path2 LIMIT 10} RETURN path, path2",
+                 "CALL {MATCH path = (c:Property {value: 'promotion_winmart'})-[:INIT]->(d:Promotion) RETURN path LIMIT 10} CALL {MATCH path2 = (a:Property {value: 'product_711'})-[:INIT]->(b:Product) RETURN path2 LIMIT 10} RETURN path, path2"]
 
 
-cypher_test = """MATCH path = (a:Property {value: 'promotion_711'})-[:INIT]->(b:Promotion)
-RETURN path"""
+cypher_test = ["MATCH path = (a:Property {value: 'price_711'})-[:INIT]->(b:Price) MATCH path2 = (c:Property {value: 'product_711'})-[:INIT]->(d:Product) RETURN path, path2 LIMIT 20",
+                 "MATCH path = (a:Property {value: 'price_711'})-[:INIT]->(b:Price) MATCH path2 = (c:Property {value: 'product_711'})-[:INIT]->(d:Product) RETURN path, path2",
+                 "MATCH path = (a:Property {value: 'product_winmart'})-[:INIT]->(b:Product) MATCH path2 = (c:Property {value: 'product_winmart'})-[:INIT]->(d:Product) RETURN path, path2 LIMIT 30",
+                 "MATCH path = (a:Property {value: 'product_winmart'})-[:INIT]->(b:Product) MATCH path2 = (c:Property {value: 'price_winmart'})-[:INIT]->(d:Price) RETURN path, path2",
+                 "MATCH path = (a:Property {value: 'promotion_711'})-[:INIT]->(b:Promotion) MATCH path = (a:Property {value: 'product_711'})-[:INIT]->(b:Product) RETURN path, path2 LIMIT 10",
+                 "MATCH path = (a:Property {value: 'promotion_winmart'})-[:INIT]->(b:Promotion) MATCH path = (a:Property {value: 'product_winmart'})-[:INIT]->(b:Product) RETURN path, path2 LIMIT 10"]
 
 def generate_context(prompt, context_data='generated'):
     context = []
@@ -77,7 +85,7 @@ if 'user_input' not in st.session_state:
 if 'cypher' not in st.session_state:
     st.session_state['cypher'] = []
 if 'index' not in st.session_state:
-    st.session_state['index'] = 4
+    st.session_state['index'] = 6
 
 
 def get_text():
@@ -111,7 +119,7 @@ if user_input:
     # else:
         # Query the database, user ID is hardcoded
 
-    if st.session_state['index'] >= 4:
+    if st.session_state['index'] >= 6:
         cypher = generate_cypher(generate_context(user_input, 'database_results'))
         if not "MATCH" in cypher:
             print('No Cypher was returned')
@@ -122,6 +130,7 @@ if user_input:
                 "No Cypher statement was generated")
             st.session_state.database_results.append("")
         else:
+            print('Cypher was returned')
             results = run_query(cypher, {'userId': USER_ID})
             # Harcode result limit to 10
             results = results[:5]
@@ -130,22 +139,20 @@ if user_input:
                 f"Question was {user_input} and the response should include only information that is given here: {str(results)}"))
             st.session_state.database_results.append(str(results))
             st.session_state.user_input.append(user_input)
-            # st.session_state.generated.append(answer)
-            # st.session_state.cypher.append(cypher_test)
             st.session_state.generated.append(answer)
             st.session_state.cypher.append(cypher)
 
     else:
-        results = run_query(cypher_output[i], {'userId': USER_ID})
+        results = run_query(cypher_output[st.session_state['index']], {'userId': USER_ID})
         # Harcode result limit to 10
-        results = results[:10]
+        results = results[:20]
         # Graph2text
         answer = generate_response(generate_context(
             f"Question was {user_input} and the response should include only information that is given here: {str(results)}"))
         st.session_state.database_results.append(str(results))
         st.session_state.user_input.append(user_input)
         # st.session_state.generated.append(answer)
-        # st.session_state.cypher.append(cypher_test)
+        # st.session_state.cypher.append(cypher_test[st.session_state['index']])
         st.session_state.generated.append(responses[st.session_state['index']])
         st.session_state.cypher.append(cypher_output[st.session_state['index']])
         st.session_state['index'] += 1
